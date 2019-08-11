21147666fbd4401d70b35ec0543bcb8a38ae88c6
AHCI21 <ahci21@gmail.com>
2019-08-11 02:20:43 -0500

    Funcion Cotizador Completa

--- app/src/main/java/com/haras/tools/Cotizador.kt
+++ app/src/main/java/com/haras/tools/Cotizador.kt
@@ -3,6 +3,9 @@ package com.haras.tools
 import android.net.TrafficStats
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import android.widget.CompoundButton
 import android.widget.Switch
 import android.widget.Toast
@@ -11,57 +14,109 @@ import java.math.RoundingMode
 import java.text.DecimalFormat
 
 class Cotizador : AppCompatActivity() {
+
+    private var cantidadInicial : Float = 0f
+    private var cantidadInicialAlterna : Float = 0f
+    private var cantidadFinal : Float = 0f
+    private var usd : Float = 1f
+    private var iva : Float = 1.16f
+    private var ganancia : Float = 1f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cotizador)
 
 
+        editCantidad.addTextChangedListener(object:TextWatcher{
+            override fun afterTextChanged(s: Editable) {}
 
-
-        /*btnCalcular.setOnClickListener {
-            var cotizacion : Float = 0f
-            var ivaCompra: Float = 0f
-
-
-            try {
-                ivaCompra = editCantidad.text.toString().toFloat()*1.16f
-            }catch (e: Throwable){
-                Toast.makeText(this,"Ingrese un valor valido",Toast.LENGTH_LONG).show()
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
             }
 
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                cotizar()
+            }
+        })
 
-            if(swIvaVenta.isChecked){
-                var ivaVenta: Float = ivaCompra * 1.16f
-                cotizacion = ivaVenta * ((editGanancia.text.toString().toFloat()/100)+1)
-            }else{
-                cotizacion = ivaCompra * ((editGanancia.text.toString().toFloat()/100)+1)
+        editPorcentaje.addTextChangedListener(object:TextWatcher{
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
             }
 
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                try {
+                    ganancia = (editPorcentaje.text.toString().toFloat()/100f)+1f
+                }catch (e:Throwable){
+                    ganancia = 1f
+                }
 
-            txtValorFinal.text = cotizacion.toString()
-            var ganancia : Float = cotizacion - ivaCompra
-            var stringG : String = "Ganancia en Pesos: \$" + ganancia.toString()
-            txtGananciaFinal.text = stringG
-            //Actualizado
+                cotizar()
+            }
+        })
+
+        swUnit.setOnClickListener{
+            calcularDolares()
         }
-        */
+
+        swIVA.setOnClickListener{
+            calcularIVA()
+        }
+
+        //Toast.makeText(this,"Ingrese un valor valido",Toast.LENGTH_LONG).show()
+
 
     }
 
-    private fun realizarOperaciones(){
+    private fun cotizar(){
+
+
+
+        try {
+            cantidadInicial = editCantidad.text.toString().toFloat() * usd * iva
+
+            if(swIVA.isChecked){
+                cantidadInicialAlterna = cantidadInicial
+                cantidadInicial = cantidadInicialAlterna * iva
+                cantidadFinal = cantidadInicial * ganancia
+                txtGananciaAlterna.setText("Ganancia si conservas IVA extra: $"+(cantidadFinal-cantidadInicialAlterna))
+            }else{
+                cantidadFinal = cantidadInicial * ganancia
+                txtGananciaAlterna.setText("")
+            }
+        }catch (e:Throwable){
+            cantidadInicial = 0f
+            cantidadFinal = 0f
+        }
+
+        txtCompra.setText("$"+cantidadInicial)
+        txtVenta.setText("$"+cantidadFinal)
+        txtGanancia.setText("$"+(cantidadFinal-cantidadInicial))
+
 
     }
 
     private fun calcularDolares(){
-
+        if (swUnit.isChecked){
+            usd = 20f
+            cotizar()
+        }else if(!swUnit.isChecked){
+            usd = 1f
+            cotizar()
+        }
     }
 
     private fun calcularIVA(){
-
+        //if (swIVA.isChecked){
+            //iva = 1.3456f
+            cotizar()
+        //}else if(!swIVA.isChecked){
+        //    iva = 1.16f
+        //    cotizar()
+        //}
     }
-
-    private fun calcularGanancia(){
-
-    }
-
 }
